88327bdd76d6492119bb498c866b5ecfe5904331
Trapsilo Bumi <tbumi@outlook.com>
2016-05-01 21:57:22 +0900

    Add initiate_job and get_job_output scripts.

---
+++ get_job_output.py
@@ -0,0 +1,61 @@
+import boto3
+import argparse
+import sys
+import json
+import pprint
+
+
+def main():
+    glacier = boto3.client('glacier')
+    args = parse_args()
+
+    vault_name = args['vault_name']
+    job_id = args['job_id']
+
+    print('Checking job status...')
+
+    response = glacier.describe_job(
+        vaultName=vault_name,
+        jobId=job_id)
+
+    job_status_code = response['StatusCode']
+
+    print('Job status: %s' % job_status_code)
+
+    if not response['Completed']:
+        print('Exiting.')
+        sys.exit(0)
+    else:
+        print('Retrieving job data...')
+        response = glacier.get_job_output(
+            vaultName=vault_name,
+            jobId=job_id)
+
+        if response['contentType'] == 'application/json':
+            inventory_json = json.load(response['body'])
+            pprint.pprint(inventory_json)
+        elif response['contentType'] == 'text/csv':
+            print(response['body'].read())
+        else:
+            with open(args['file_name'], 'xb') as file:
+                file.write(response['body'].read())
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Get the output of a glacier job.')
+
+    parser.add_argument('-v', '--vault-name', required=True,
+                        help='The name of the vault to upload to')
+    parser.add_argument('-j', '--job-id', required=True,
+                        help='Job ID')
+    parser.add_argument('-f', '--file-name', default='glacier_archive.tar.xz',
+                        help='File name of archive to be saved, '
+                             'if the job is an archive-retrieval')
+
+    args = vars(parser.parse_args())
+
+    return args
+
+if __name__ == '__main__':
+    main()
---
+++ initiate_job.py
@@ -0,0 +1,59 @@
+import boto3
+import argparse
+
+
+def main():
+    glacier = boto3.client('glacier')
+    args = parse_args()
+
+    if args['job_type'] == 'inv' or args['job_type'] == 'inventory-retrieval':
+        job_type = 'inventory-retrieval'
+    else:
+        job_type = 'archive-retrieval'
+
+    job_params = {
+        'Format': args['format'],
+        'Type': job_type,
+        'Description': args['description']
+    }
+
+    if job_type == 'archive-retrieval':
+        job_params['ArchiveId'] = args['archive_id']
+
+    print('Sending job initiation request...')
+
+    response = glacier.initiate_job(
+        vaultName=args['vault_name'],
+        jobParameters=job_params)
+
+    print('Job initiation request recieved. Job ID: %s' % response['jobId'])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Initiate a glacier job.')
+
+    parser.add_argument('job_type', choices=['arc', 'archive-retrieval',
+                                             'inv', 'inventory-retrieval'],
+                        help='The type of job: archive-retrieval or '
+                             'inventory-retrieval')
+    parser.add_argument('-v', '--vault-name', required=True,
+                        help='The name of the vault to upload to')
+    parser.add_argument('-f', '--format', default='JSON',
+                        help='Format to request from glacier')
+    parser.add_argument('-d', '--description',
+                        help='Description of this job (optional)')
+    parser.add_argument('-a', '--archive-id',
+                        help='ID of the archive to retrieve')
+
+    args = vars(parser.parse_args())
+
+    if args['job_type'] == 'arc' or args['job_type'] == 'archive-retrieval':
+        if args['archive_id'] is None:
+            raise ValueError('For archive-retrieval jobs, '
+                             'provide archive id with argument "-a".')
+
+    return args
+
+if __name__ == '__main__':
+    main()
