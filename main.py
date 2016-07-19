25214a66214d57356ed3a9a597d8bf5a4b221313
Trapsilo Bumi <tbumi@outlook.com>
2016-07-19 22:42:45 +0900

    Add feature: resume uploads
    Also:
    - rename calculate_tree_hash function
    - add percentage indicator

--- main.py
+++ main.py
@@ -59,29 +59,73 @@ def main():
             body=file_to_upload)
 
         print('Uploaded.')
-        print('Glacier total tree hash: %s' % response['checksum'])
+        print('Glacier tree hash: %s' % response['checksum'])
         print('Location: %s' % response['location'])
         print('Archive ID: %s' % response['archiveId'])
+        print('Done.')
+        file_to_upload.close()
         return
 
     job_list = []
     list_of_checksums = []
-    for byte_pos in range(0, file_size, part_size):
-        job_list.append(byte_pos)
-        list_of_checksums.append(None)
 
-    num_parts = len(job_list)
+    if args['upload_id'] is None:
+        print('Initiating multipart upload...')
+        response = glacier.initiate_multipart_upload(
+            vaultName=vault_name,
+            archiveDescription=args['arc_desc'],
+            partSize=str(part_size)
+            )
+        upload_id = response['uploadId']
 
-    print('File size is %d bytes. Will upload in %d parts.' %
-          (file_size, num_parts))
+        for byte_pos in range(0, file_size, part_size):
+            job_list.append(byte_pos)
+            list_of_checksums.append(None)
 
-    print('Initiating multipart upload...')
-    response = glacier.initiate_multipart_upload(
-        vaultName=vault_name,
-        archiveDescription=args['arc_desc'],
-        partSize=str(part_size)
+        num_parts = len(job_list)
+        print('File size is %d bytes. Will upload in %d parts.' %
+              (file_size, num_parts))
+    else:
+        print('Resuming upload...')
+        upload_id = args['upload_id']
+
+        print('Fetching already uploaded parts...')
+        response = glacier.list_parts(
+            vaultName=vault_name,
+            uploadId=upload_id
         )
-    upload_id = response['uploadId']
+        parts = response['Parts']
+        part_size = response['PartSizeInBytes']
+        while 'Marker' in response:
+            print('Getting more parts...')
+            response = glacier.list_parts(
+                vaultName=vault_name,
+                uploadId=upload_id,
+                marker=response['Marker']
+            )
+            parts.extend(response['Parts'])
+
+        for byte_pos in range(0, file_size, part_size):
+            job_list.append(byte_pos)
+            list_of_checksums.append(None)
+        num_parts = len(job_list)
+
+        for part_data in parts:
+            byte_start = int(part_data['RangeInBytes'].partition('-')[0])
+            sha256_treehash = part_data['SHA256TreeHash']
+
+            part_num = int(byte_start / part_size)
+            print('Checking part %s of %s...' % (part_num + 1, len(parts)))
+
+            file_to_upload.seek(byte_start)
+            part = file_to_upload.read(part_size)
+            checksum = calculate_tree_hash(part, part_size)
+
+            if checksum == sha256_treehash:
+                print('Checksum match. Removing from job list.')
+                job_list.remove(byte_start)
+            else:
+                print('Checksum mismatch. Not removing.')
 
     print('Spawning threads...')
     with concurrent.futures.ThreadPoolExecutor(
@@ -99,16 +143,27 @@ def main():
                 e = future.exception()
                 if e is not None:
                     print('Exception occured: %r' % e)
-            glacier.abort_multipart_upload(
-                vaultName=vault_name, uploadId=upload_id)
-            print('Upload aborted.')
+            print('Upload not aborted. Upload id: %s' % upload_id)
+            print('Exiting.')
+            file_to_upload.close()
             sys.exit(1)
         else:
             # all threads completed without raising
             for future in done:
-                list_of_checksums[futures_list[future]] = future.result()
+                job_index = futures_list[future]
+                list_of_checksums[job_index] = future.result()
 
-    total_tree_hash = calculate_tree_hash(list_of_checksums)
+    if len(list_of_checksums) != num_parts:
+        print('List of checksums incomplete. Recalculating...')
+        list_of_checksums = []
+        for byte_pos in range(0, file_size, part_size):
+            part_num = int(byte_pos / part_size)
+            print('Checksum %s of %s...' % (part_num + 1, num_parts))
+            file_to_upload.seek(byte_pos)
+            part = file_to_upload.read(part_size)
+            list_of_checksums.append(calculate_tree_hash(part, part_size))
+
+    total_tree_hash = calculate_total_tree_hash(list_of_checksums)
 
     print('Completing multipart upload...')
     response = glacier.complete_multipart_upload(
@@ -119,6 +174,7 @@ def main():
     print('Glacier total tree hash: %s' % response['checksum'])
     print('Location: %s' % response['location'])
     print('Archive ID: %s' % response['archiveId'])
+    print('Done.')
     file_to_upload.close()
 
 
@@ -132,28 +188,34 @@ def upload_part(byte_pos, vault_name, upload_id, part_size, fileobj, file_size,
     range_header = 'bytes {}-{}/{}'.format(
         byte_pos, byte_pos + len(part) - 1, file_size)
     part_num = int(byte_pos / part_size)
+    percentage = part_num/num_parts
 
-    print('Uploading part %s of %s...' %
-          (part_num + 1, num_parts))
+    print('Uploading part {0} of {1}... ({2:.2%})'.format(
+        part_num + 1, num_parts, percentage))
 
     response = glacier.upload_multipart_part(
         vaultName=vault_name, uploadId=upload_id, range=range_header,
         body=part)
 
-    checksums = []
-    step = 1024 * 1024  # 1 MB
-    upper_bound = min(len(part), part_size)
-    for chunk_pos in range(0, upper_bound, step):
-        chunk = part[chunk_pos:chunk_pos+step]
-        checksums.append(hashlib.sha256(chunk).hexdigest())
-    checksum = calculate_tree_hash(checksums)
-
+    checksum = calculate_tree_hash(part, part_size)
+    del part
     assert checksum == response['checksum'], 'Checksums do not match.'
 
     return checksum
 
 
-def calculate_tree_hash(list_of_checksums):
+def calculate_tree_hash(part, part_size):
+    checksums = []
+    upper_bound = min(len(part), part_size)
+    step = 1024 * 1024  # 1 MB
+    for chunk_pos in range(0, upper_bound, step):
+        chunk = part[chunk_pos:chunk_pos+step]
+        checksums.append(hashlib.sha256(chunk).hexdigest())
+        del chunk
+    return calculate_total_tree_hash(checksums)
+
+
+def calculate_total_tree_hash(list_of_checksums):
     tree = list_of_checksums[:]
     while len(tree) > 1:
         parent = []
@@ -185,6 +247,9 @@ def parse_args():
                         'megabytes (e.g. 1, 2, 4, 8) default: 8')
     parser.add_argument('-t', '--num-threads', type=int, default=5,
                         help='The amount of concurrent threads (default: 5)')
+    parser.add_argument('-u', '--upload-id',
+                        help='Optional upload id, if provided then will '
+                              'resume upload.')
 
     args = vars(parser.parse_args())
 
